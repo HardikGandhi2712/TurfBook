7df8f665dda9e96bd0b5ab6ec0930aff45f1c0f9
HardikGandhi2712 <112877053+HardikGandhi2712@users.noreply.github.com>
2026-08-10 20:13:46 +0530

    Add routes for CourtRegistrationForm and CenterDashboard

--- turfbook_react/supportform/src/App.jsx
+++ turfbook_react/supportform/src/App.jsx
@@ -1,12 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import SupportForm from "./SupportForm";
 import AdminDashboard from "./AdminDashboard";
+import CourtRegistrationForm from "./CourtRegistrationForm";
+import CenterDashboard from "./CenterDashboard";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<SupportForm />} />
+      <Route path="/court" element={<CourtRegistrationForm />} />
       <Route path="/admin" element={<AdminDashboard />} />
+      <Route path="/admin/court" element={<CenterDashboard />} />
     </Routes>
   );
 }
